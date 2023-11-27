df7d52652aec43c13d405977ed6c759fa5b14262
nikunj rathore <nikunjrathore06@gmail.com>
2023-11-27 15:50:06 +0530

    adding order tracker for customers

--- Ecommers/settings.py
+++ Ecommers/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Ecommers.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Ecommers/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
